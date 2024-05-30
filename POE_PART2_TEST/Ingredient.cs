9a80ddfaebef3e941949f1de31104899931a7563
Steph kib <kbst123@outlook.fr>
2024-05-30 18:19:53 +0200

    Adding a test ingredient c# file with the same attributes and methods as the main project

---
+++ POE_PART2_TEST/Ingredient.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POE_PART2_TEST
+{
+    public class Ingredient
+    {
+        public string Name { get; }
+        public double Quantity { get; set; }
+        public string Unit { get; }
+        public double Calories { get; }
+        public string FoodGroup { get; }
+        private double originalQuantity;
+
+        // Constructor to initialize the ingredient
+        public Ingredient(string name, double quantity, string unit, double calories, string foodGroup)
+        {
+            Name = name;
+            Quantity = quantity;
+            Unit = unit;
+            Calories = calories;
+            FoodGroup = foodGroup;
+            originalQuantity = quantity;
+        }
+
+        // Method to reset the ingredient quantity
+        public void ResetQuantity()
+        {
+            Quantity = originalQuantity;
+        }
+    }
+}
