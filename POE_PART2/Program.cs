1356151113b81b47b3566d9727d700f7100924db
Steph kib <kbst123@outlook.fr>
2024-05-30 18:04:48 +0200

    Add project files.

---
+++ POE_PART2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POE_PART2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
