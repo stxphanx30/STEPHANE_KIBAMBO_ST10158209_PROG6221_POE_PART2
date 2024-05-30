435d418eb20545910cb392e13a0fab3a14ba5276
Steph kib <kbst123@outlook.fr>
2024-05-30 18:21:33 +0200

    Adding recipe test class and method for the unit testing

---
+++ POE_PART2_TEST/Recipe.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POE_PART2_TEST
+{
+    public class Recipe
+    {
+        public string Name { get; set; }
+        public List<Ingredient> Ingredients { get; set; } = new List<Ingredient>(); // List to store ingredients
+
+        // Method to calculate total calories of the recipe
+        public double CalculateTotalCalories()
+        {
+            return Ingredients.Sum(ingredient => ingredient.Calories + ingredient.Quantity);
+        }
+    }
+}
