3d91ee27a564ab7e786d611657c89c2ca504c8a5
Steph kib <kbst123@outlook.fr>
2024-05-30 18:16:39 +0200

    Creating a test folder

---
+++ POE_PART2_TEST/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace POE_PART2_TEST
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
