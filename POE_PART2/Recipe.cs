8bd2379ffe46a69379facd8f4217121403463d11
Steph kib <kbst123@outlook.fr>
2024-05-30 18:14:30 +0200

    mistake on adding class recipe corrected

--- POE_PART2/Recipe.cs
+++ POE_PART2/Recipe.cs
@@ -7,102 +7,105 @@ using System.Xml.Linq;
 
 namespace POE_PART2
 {
-    public string Name { get; set; }
-    public List<Ingredient> Ingredients { get; set; } = new List<Ingredient>(); // List to store ingredients
-    private List<string> stepDescriptions = new List<string>(); // List to store steps
-
-    // Method to get recipe details from user input
-    public void GetRecipeDetails()
+    class Recipe
     {
-        Console.WriteLine("Please enter the name of the recipe:");
-        Name = Console.ReadLine();
+        public string Name { get; set; }
+        public List<Ingredient> Ingredients { get; set; } = new List<Ingredient>(); // List to store ingredients
+        private List<string> stepDescriptions = new List<string>(); // List to store steps
 
-        GetIngredients(); // Get ingredients from user input
-        GetSteps(); // Get steps from user input
-    }
-
-    // Method to get ingredients from user input
-    public void GetIngredients()
-    {
-        Console.WriteLine("Please enter the number of ingredients:");
-        int ingredientCount = int.Parse(Console.ReadLine());
-
-        for (int i = 0; i < ingredientCount; i++)
+        // Method to get recipe details from user input
+        public void GetRecipeDetails()
         {
-            Console.WriteLine($"Enter details for ingredient {i + 1}:");
-            Console.Write("Name: ");
-            string name = Console.ReadLine();
-            Console.Write("Quantity: ");
-            double quantity = double.Parse(Console.ReadLine());
-            Console.Write("Unit: ");
-            string unit = Console.ReadLine();
-            Console.Write("Calories: ");
-            double calories = double.Parse(Console.ReadLine());
-            Console.Write("Food Group: ");
-            string foodGroup = Console.ReadLine();
+            Console.WriteLine("Please enter the name of the recipe:");
+            Name = Console.ReadLine();
 
-            Ingredients.Add(new Ingredient(name, quantity, unit, calories, foodGroup));
+            GetIngredients(); // Get ingredients from user input
+            GetSteps(); // Get steps from user input
         }
-    }
 
-    // Method to get steps from user input
-    public void GetSteps()
-    {
-        Console.WriteLine("Enter the number of steps:");
-        int stepCount = int.Parse(Console.ReadLine());
-
-        for (int i = 0; i < stepCount; i++)
+        // Method to get ingredients from user input
+        public void GetIngredients()
         {
-            Console.WriteLine($"Enter description for step {i + 1}:");
-            stepDescriptions.Add(Console.ReadLine());
-        }
-    }
+            Console.WriteLine("Please enter the number of ingredients:");
+            int ingredientCount = int.Parse(Console.ReadLine());
 
-    // Method to display the recipe
-    public void DisplayRecipe()
-    {
-        Console.WriteLine($"\nRecipe: {Name}");
-        Console.WriteLine("Ingredients:");
-        foreach (var ingredient in Ingredients)
+            for (int i = 0; i < ingredientCount; i++)
+            {
+                Console.WriteLine($"Enter details for ingredient {i + 1}:");
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Quantity: ");
+                double quantity = double.Parse(Console.ReadLine());
+                Console.Write("Unit: ");
+                string unit = Console.ReadLine();
+                Console.Write("Calories: ");
+                double calories = double.Parse(Console.ReadLine());
+                Console.Write("Food Group: ");
+                string foodGroup = Console.ReadLine();
+
+                Ingredients.Add(new Ingredient(name, quantity, unit, calories, foodGroup));
+            }
+        }
+
+        // Method to get steps from user input
+        public void GetSteps()
         {
-            Console.WriteLine($"- {ingredient.Quantity} {ingredient.Unit} of {ingredient.Name} ({ingredient.Calories} calories, {ingredient.FoodGroup})");
+            Console.WriteLine("Enter the number of steps:");
+            int stepCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < stepCount; i++)
+            {
+                Console.WriteLine($"Enter description for step {i + 1}:");
+                stepDescriptions.Add(Console.ReadLine());
+            }
         }
-        Console.WriteLine("\nSteps:");
-        for (int i = 0; i < stepDescriptions.Count; i++)
+
+        // Method to display the recipe
+        public void DisplayRecipe()
         {
-            Console.WriteLine($"- {i + 1}. {stepDescriptions[i]}");
+            Console.WriteLine($"\nRecipe: {Name}");
+            Console.WriteLine("Ingredients:");
+            foreach (var ingredient in Ingredients)
+            {
+                Console.WriteLine($"- {ingredient.Quantity} {ingredient.Unit} of {ingredient.Name} ({ingredient.Calories} calories, {ingredient.FoodGroup})");
+            }
+            Console.WriteLine("\nSteps:");
+            for (int i = 0; i < stepDescriptions.Count; i++)
+            {
+                Console.WriteLine($"- {i + 1}. {stepDescriptions[i]}");
+            }
+            Console.WriteLine($"\nTotal Calories: {CalculateTotalCalories()}");
         }
-        Console.WriteLine($"\nTotal Calories: {CalculateTotalCalories()}");
-    }
 
-    // Method to calculate total calories of the recipe
-    public double CalculateTotalCalories()
-    {
-        return Ingredients.Sum(ingredient => ingredient.Calories + ingredient.Quantity);
-    }
-
-    // Method to scale the recipe quantities
-    public void ScaleRecipe(double factor)
-    {
-        foreach (var ingredient in Ingredients)
+        // Method to calculate total calories of the recipe
+        public double CalculateTotalCalories()
         {
-            ingredient.Quantity *= factor;
+            return Ingredients.Sum(ingredient => ingredient.Calories + ingredient.Quantity);
         }
-    }
 
-    // Method to reset the ingredient quantities
-    public void ResetQuantities()
-    {
-        foreach (var ingredient in Ingredients)
+        // Method to scale the recipe quantities
+        public void ScaleRecipe(double factor)
         {
-            ingredient.ResetQuantity();
+            foreach (var ingredient in Ingredients)
+            {
+                ingredient.Quantity *= factor;
+            }
         }
-    }
 
-    // Method to clear the recipe data
-    public void ClearData()
-    {
-        Ingredients.Clear();
-        stepDescriptions.Clear();
+        // Method to reset the ingredient quantities
+        public void ResetQuantities()
+        {
+            foreach (var ingredient in Ingredients)
+            {
+                ingredient.ResetQuantity();
+            }
+        }
+
+        // Method to clear the recipe data
+        public void ClearData()
+        {
+            Ingredients.Clear();
+            stepDescriptions.Clear();
+        }
     }
 }
